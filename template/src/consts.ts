a3dfe0ca64114c0fbb9001e3c71d5b9918350a27
CN059 <cn059@outlook.com>
2026-03-31 19:24:10 +0800

    refactor: 把参考的博客模板删了

--- template/src/consts.ts
+++
@@ -1,5 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "CN059 的技术博客";
-export const SITE_DESCRIPTION = "欢迎来到我的网站";
